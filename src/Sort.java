ceb4bc5a935ee27c94c031bcf62cbb9f257ea4f3
bikramsubedi <bxs1561@g.rit.edu>
2020-06-23 20:37:19 -0400

    insertion sort

--- src/Sort.java
+++ src/Sort.java
@@ -1,7 +1,7 @@
 import java.util.Arrays;
 //Worst and Average Case Complexity: O(n*n). Worst case occurs when array is reverse sorted.
 //Best Case Time Complexity: O(n). Best case occurs when array is already sorted
-public class BubbleSort {
+public class Sort {
     public static void bubbleSort(int[]arr){
         System.out.println("unsorted arrays: "+" "+Arrays.toString(arr));
         //flag to optimized sort algorithm
